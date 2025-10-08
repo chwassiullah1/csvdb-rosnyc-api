3c8a34874b3ccd6c8faa03c245beba3acaf1e317
Hamza Syed <80567400+hamza-140@users.noreply.github.com>
2025-10-08 13:38:13 +0500

    Update Celery broker and result backend settings to use RabbitMQ

--- edwin_backend/settings.py
+++ edwin_backend/settings.py
@@ -140,6 +140,5 @@ MEDIA_URL = '/files/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CELERY_BROKER_URL = "redis://localhost:6379/0"
-CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
-
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost//'
+CELERY_RESULT_BACKEND = 'rpc://'
